e5bad2541d753c92cdd297d52b4bfdff2848ad1b
lawlietrzk <143306662+lawlietrzk@users.noreply.github.com>
2023-11-26 18:59:30 +0000

    cl

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import nltk
 import spacy
+
+import subprocess
+
+subprocess.run([f"{sys.executable}", "-m", "spacy", "download", "en_core_web_sm"])
+
 nltk.download('stopwords')
 spacy.load('en_core_web_sm')
 
